3bf42b522f5de5998e835d4f7deb403e4f7dede3
Devin J. Pohly <djpohly@gmail.com>
2014-02-03 23:11:47 -0500

    more data progress

---
+++ data/0a8b71.asm
@@ -0,0 +1,4 @@
+0a8b71    798b         .da $0a8b79
+0a8b73    7f8b         .da $0a8b7f
+0a8b75    858b         .da $0a8b85
+0a8b77    8b8b         .da $0a8b8b
---
+++ data/0a8b91.asm
@@ -0,0 +1,4 @@
+0a8b91    fa17         .dw $17fa
+0a8b93    ff13         .dw $13ff
+0a8b95    1f01         .dw $011f
+0a8b97    fa17         .dw $17fa
