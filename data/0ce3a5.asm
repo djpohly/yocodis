74aeccdf45744077e77ea33f2ce28d5bf6437772
Devin J. Pohly <djpohly@gmail.com>
2014-02-05 09:08:47 -0500

    and that finishes the data/0c we have so far

---
+++ data/0ce3a5.asm
@@ -0,0 +1,17 @@
+0ce3a5    00001a3f0c
+0ce3aa    0000122f0c
+0ce3af    00000a1f0c
+0ce3b4    0000020f0c
+0ce3b9    01f0122d0c
+0ce3be    01f0020d0c
+0ce3c3    00d6034c4c
+0ce3c8    00de034b4c
+0ce3cd    00e603494c
+0ce3d2    01d60b5b4c
+0ce3d7    01e60b594c
+0ce3dc    001a034c0c
+0ce3e1    0012034b0c
+0ce3e6    000a03490c
+0ce3eb    01120b5b0c
+0ce3f0    01020b590c
+0ce3f5    80
---
+++ data/0ce53e.asm
@@ -0,0 +1,2 @@
+0ce53e    42e5         .da $0ce542
+0ce540    f2e5         .da $0ce5f2
