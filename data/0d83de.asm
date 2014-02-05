1aa1d59848a122530dc55695cdaf6a2090dbab80
Devin J. Pohly <djpohly@gmail.com>
2014-02-05 09:26:42 -0500

    first pass done save for mystery 07 bank

---
+++ data/0d83de.asm
@@ -0,0 +1,2 @@
+0d83de    cd001ffe     .db cd 00 1f fe
+0d83e1    ff           .db ff
---
+++ data/0fbd45.asm
@@ -0,0 +1,5 @@
+0fbd45    c000         .dw $00c0
+0fbd47    c003         .dw $03c0
+0fbd49    c005         .dw $05c0
+0fbd4b    c007         .dw $07c0
+0fbd4d    c009         .dw $09c0
