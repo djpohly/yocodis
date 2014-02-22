52b0397b420d2cb4639c09a2baf2885efe748090
Devin J. Pohly <djpohly@gmail.com>
2014-02-22 16:50:33 -0500

    fix up some of these old 5s

--- data/0acd5b.asm
+++ data/0acd5b.asm
@@ -1,29 +1,31 @@
-0acd5b    00fcb875     .db $00,$fc,$b8,$75
-0acd5f    2600fcc0     .db $26,$00,$fc,$c0
-0acd63    752600fc     .db $75,$26,$00,$fc
-0acd67    c8752600     .db $c8,$75,$26,$00
-0acd6b    fcd07526     .db $fc,$d0,$75,$26
-0acd6f    00fcd875     .db $00,$fc,$d8,$75
-0acd73    2600fce0     .db $26,$00,$fc,$e0
-0acd77    752600fc     .db $75,$26,$00,$fc
-0acd7b    e8752600     .db $e8,$75,$26,$00
-0acd7f    fcf07526     .db $fc,$f0,$75,$26
-0acd83    00fcf875     .db $00,$fc,$f8,$75
-0acd87    268000fc     .db $26,$80,$00,$fc
-0acd8b    fc742600     .db $fc,$74,$26,$00
-0acd8f    fc047526     .db $fc,$04,$75,$26
-0acd93    8000f6fc     .db $80,$00,$f6,$fc
-0acd97    78360000     .db $78,$36,$00,$00
-0acd9b    fc783600     .db $fc,$78,$36,$00
-0acd9f    fef47836     .db $fe,$f4,$78,$36
-0acda3    00fcfa77     .db $00,$fc,$fa,$77
-0acda7    368000ff     .db $36,$80,$00,$ff
-0acdab    f5783600     .db $f5,$78,$36,$00
-0acdaf    f7f77836     .db $f7,$f7,$78,$36
-0acdb3    00fcfa76     .db $00,$fc,$fa,$76
-0acdb7    368000f7     .db $36,$80,$00,$f7
-0acdbb    f6783600     .db $f6,$78,$36,$00
-0acdbf    fff57836     .db $ff,$f5,$78,$36
-0acdc3    00fcfc78     .db $00,$fc,$fc,$78
-0acdc7    3600fcfa     .db $36,$00,$fc,$fa
-0acdcb    783680       .db $78,$36,$80
+0acd5b    00fcb87526
+0acd60    00fcc07526
+0acd65    00fcc87526
+0acd6a    00fcd07526
+0acd6f    00fcd87526
+0acd74    00fce07526
+0acd79    00fce87526
+0acd7e    00fcf07526
+0acd83    00fcf87526
+0acd88    80
+
+0acd89    00fcfc7426
+0acd8e    00fc047526
+0acd93    80
+
+0acd94    00f6fc7836
+0acd99    0000fc7836
+0acd9e    00fef47836
+0acda3    00fcfa7736
+0acda8    80
+
+0acda9    00fff57836
+0acdae    00f7f77836
+0acdb3    00fcfa7636
+0acdb8    80
+
+0acdb9    00f7f67836
+0acdbe    00fff57836
+0acdc3    00fcfc7836
+0acdc8    00fcfa7836
+0acdcd    80
--- data/0ad911.asm
+++ data/0ad911.asm
@@ -1,8 +1,8 @@
 0ad911    15d9         .da $0ad915
 0ad913    1bd9         .da $0ad91b
-0ad915    01f8         .dw $f801
-0ad917    f8ea         .dw $eaf8
-0ad919    3780         .dw $8037
-0ad91b    01f8         .dw $f801
-0ad91d    f8ed         .dw $edf8
-0ad91f    3780         .dw $8037
+
+0ad915    01f8f8ea37
+0ad91a    80
+
+0ad91b    01f8f8ed37
+0ad920    80
